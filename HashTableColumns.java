18eb6b07e1a31edb821aa424817e441dcd25f8f8
Lisa Maria Dmello <lisa.dmello1994@gmail.com>
2016-11-30 02:31:17 -0800

    Add files via upload

---
+++ HashTableColumns.java
@@ -0,0 +1,13 @@
+package resolution;
+
+public class HashTableColumns {
+	public String positive;
+	public String negative;
+	public String sentence;
+
+	public HashTableColumns(String positive, String negative, String sentence) {
+		this.positive = positive;
+		this.negative = negative;
+		this.sentence=sentence;
+	}
+}
